828407d496d7d810894e05412a7bc3c25c43add7
pablopunk <yo@pablo.pink>
2020-09-08 01:34:40 +0200

    New personal domain

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default class extends React.Component {
       <div>
         <p>Welcome.</p>
         <p>
-          You can go to <a href="https://pablo.pink">my personal website</a>{' '}
+          You can go to <a href="https://pablo.im">my personal website</a>{' '}
           instead.
         </p>
         <h1>Latest projects on GitHub</h1>
